d3a5becf8cce17c43292b063d60893d8b0d35559
Asma <frankyn323@gmail.com>
2026-03-23 14:43:22 +0400

    Add rooms to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,13 +5,16 @@ function Navbar() {
   return (
     <nav style={{ padding: "15px", background: "#12284b", color: "#bfb6ce" }}>
 
-      <Link to="/" style={{ color: "#bfb6ce", marginRight: "15px" }}>
+      <Link to="/Home" style={{ color: "#bfb6ce", marginRight: "15px" }}>
         Home
       </Link>
-      <Link to="/login" style={{ color: "#bfb6ce", marginRight: "15px" }}>
+      <Link to="/Rooms" style={{ color: "#bfb6ce", marginRight: "15px" }}>
+        Rooms
+      </Link>
+      <Link to="/Login" style={{ color: "#bfb6ce", marginRight: "15px" }}>
         Login
       </Link>
-      <Link to="/signup" style={{ color: "#bfb6ce" }}>
+      <Link to="/Signup" style={{ color: "#bfb6ce" }}>
         Signup
       </Link>
     </nav>
---
+++ client/src/pages/Rooms.jsx
@@ -0,0 +1,5 @@
+function Rooms() {
+  return <h2>Room Booking Page</h2>;
+}
+
+export default Rooms;
