0c9ee9c51b0e1f321b9bd8af3b96b99932034a7a
Asma <frankyn323@gmail.com>
2026-03-31 22:11:26 +0400

    sidebar added, navbar updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,43 +1,52 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/LovenderWhite.png";
-import searchIcon from "../assets/searchIcon.png";
 import accountIcon from "../assets/accountIcon.png";
+import Sidebar from "./Sidebar";
 import "./Navbar.css";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header className="site-header">
-      <div className="nav-inner">
+    <>
+      <header className="site-header">
+        <div className="nav-inner">
 
-        {/* LEFT SIDE */}
-        <div className="nav-left">
-          <button className="icon-btn">☰</button>
-          
-          <Link to="/search">
-            <button className="icon-btn">
-              <img src={searchIcon} alt="search" className="icon-img" />
+          {/* LEFT SIDE */}
+          <div className="nav-left">
+            <button
+              className="icon-btn"
+              onMouseEnter={() => setIsOpen(true)}
+            >
+              ☰
             </button>
-          </Link>
+          </div>
+
+          {/* CENTER LOGO */}
+          <div className="nav-center">
+            <Link to="/">
+              <img src={logo} alt="logo" className="logo" />
+            </Link>
+          </div>
+
+          {/* RIGHT SIDE */}
+          <div className="nav-right">
+            <Link to="/Rooms" className="book-btn">Book Now</Link>
+
+            <Link to="/Account" className="icon-btn">
+              <img src={accountIcon} alt="account" className="icon-img" />
+            </Link>
+          </div>
+
         </div>
+      </header>
 
-        {/* CENTER LOGO */}
-        <div className="nav-center">
-          <Link to="/">
-            <img src={logo} alt="logo" className="logo" />
-          </Link>
-        </div>
-
-        {/* RIGHT SIDE */}
-        <div className="nav-right">
-          <Link to="/Booking" className="book-btn">Book Now</Link>
-
-          <Link to="/Account" className="icon-btn">
-            <img src={accountIcon} alt="account" className="icon-img" />
-          </Link>
-        </div>
-
-      </div>
-    </header>
+      <Sidebar
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+      />
+    </>
   );
 }
 
--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,16 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
-import Booking from "../pages/Booking";
 import Account from "../pages/Account";
-import Search from "../pages/Search";
+import Rooms from "../pages/Rooms";
+import Contact from "../pages/Contact";
+import About from "../pages/About";
+import Packages from "../pages/Packages";
+
+
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/Booking" element={<Booking />} />
       <Route path="/Account" element={<Account />} />
-      <Route path="/Search" element={<Search />} />
+      <Route path="/Rooms" element={<Rooms />} />
+      <Route path="/Packages" element={<Packages />} />
+      <Route path="/Contact" element={<Contact />} />
+      <Route path="/About" element={<About />} />
     </Routes>
   );
 }
